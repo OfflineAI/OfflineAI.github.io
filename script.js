2f5552bd209b99c9a4c3e502aa9745b6fa3eb249
OfflineAI <96028095+OfflineAI@users.noreply.github.com>
2021-12-13 19:55:12 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+mapboxgl.accessToken =
+  "pk.eyJ1Ijoic2hhZG93bWFwIiwiYSI6ImNreDVjZXA1ZjFyY3QydXBuYmZjMmpnamMifQ.kw8hu-U96ptfrsh14nycpA"
+
+navigator.geolocation.getCurrentPosition(successLocation, errorLocation, {
+  enableHighAccuracy: true
+})
+
+function successLocation(position) {
+  setupMap([position.coords.longitude, position.coords.latitude])
+}
+
+function errorLocation() {
+  setupMap([-2.24, 53.48])
+}
+
+function setupMap(center) {
+  const map = new mapboxgl.Map({
+    container: "map",
+    style: "mapbox://styles/mapbox/streets-v11",
+    center: center,
+    zoom: 15
+  })
+
+  const nav = new mapboxgl.NavigationControl()
+  map.addControl(nav)
+
+  var directions = new MapboxDirections({
+    accessToken: mapboxgl.accessToken
+  })
+
+  map.addControl(directions, "top-left")
+}
